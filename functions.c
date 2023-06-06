134f63230af31a57648384af6e6f825adb9b7b88
Oskar H <assawin.h@gmail.com>
2023-06-06 10:55:21 +0200

    I think i fixed it. I might need to run some more tests to be sure.

--- functions.c
+++ functions.c
@@ -3,13 +3,9 @@
 #include "finf.h"
 #include <string.h>
 
-
 void finf (char search[]) { // First version of finf. VERY BAD. 
     int i = 0, k = 0;
     size_t searchsz = strlen(search);
-    char temp[searchsz];
-    int foundflag = 0;
-    //int fileindex = 0;
     int startindex = 0;
     int findcount = 0;
     FILE* file = fopen("file.txt", "r");
@@ -17,11 +13,14 @@ void finf (char search[]) { // First version of finf. VERY BAD.
         printf("Failed to open the file.\n");
         return;
     }
-    //printf("Size of opt: %ld\n", searchsz);
 
-    fseek(file, 0, SEEK_END); long filesize = ftell(file); // Determine the amound of characters in a file. 
+    fseek(file, 0, SEEK_END); 
+    long filesize = ftell(file); // Determine the amound of characters in a file. 
     
     while (startindex != filesize) {
+
+        char temp[searchsz];
+        int foundflag = 0;
 
         fseek(file, startindex, SEEK_SET); // startindex
 
@@ -35,17 +34,12 @@ void finf (char search[]) { // First version of finf. VERY BAD.
 
         for (k = 0; k < searchsz; k++){ // Compare temp and search
                 if (temp[k] == search[k]) {
-                    //printf("ff%d ", foundflag);
                     foundflag++;
                     if (foundflag == searchsz) {
                         findcount++;
-                        printf("Word found.\n");
-                        foundflag = 0;    
-                    
+                        printf("Word found.\n");   
                     } // Check if the word is found or not. If the foundflag = searchsz then the word was found. 
                 } else { 
-                    //printf("fi%d ", fileindex);
-                    //fileindex++; // If the letters being compared are wrong then it breaks out of the loop.
                     break; 
                 
             }
@@ -59,14 +53,6 @@ void finf (char search[]) { // First version of finf. VERY BAD.
         printf("%c", search[m]);
     }
     printf("\" was found %d times.\n", findcount);
-
-    /*
-    for (int j = 0; j < searchsz; j++) { // Print temp
-        printf("%c", temp[j]);
-        //printf("%c", search[j]);
-    }
-    */
-
 
     fclose(file);
 
